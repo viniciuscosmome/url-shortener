c3b6444eb8372c5e3c33bcf7950220b556fae58f
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2024-01-20 23:21:22 -0300

    chore: setup class-validator

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get<string>('PORT');
+
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(port);
 }
