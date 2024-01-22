3bb5f5193d1b718a6bcd3523ca7a1b6e1a8ce894
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2024-01-22 20:32:06 -0300

    feat: update database errors

--- src/global/errors/database.errors.ts
+++ src/global/errors/database.errors.ts
@@ -1,7 +1,25 @@
 import { InternalServerErrorException } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
 
-export function handleDatabaseError(error: unknown) {
-  console.error(error);
+function sendMessage(message: string) {
+  throw new InternalServerErrorException({ message });
+}
 
-  throw new InternalServerErrorException('Erro não controlado :: db');
+const exceptions = {
+  P2002: 'O registro precisa ser único.',
+  P2007: 'O tipo de dado está diferente do esperado.',
+  P2012: 'Um valor obrigatório não foi fornecido.',
+  P2025: 'O registro não foi encontrado.',
+};
+
+export function handleDatabaseError(error: object) {
+  if (error instanceof Prisma.PrismaClientKnownRequestError) {
+    const exception = exceptions[error.code];
+
+    if (exception) {
+      sendMessage(exception);
+    }
+  }
+
+  sendMessage('Erro desconhecido.');
 }
