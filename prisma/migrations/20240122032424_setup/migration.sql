c485797fa9178f18a73d10c67da420f2870c0ddf
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2024-01-22 00:23:22 -0300

    chore: structure ShortURL model

--- prisma/migrations/20240122032424_setup/migration.sql
+++ prisma/migrations/20240122032424_setup/migration.sql
@@ -10,14 +10,18 @@ CREATE TABLE "users" (
 -- CreateTable
 CREATE TABLE "short_urls" (
     "id" TEXT NOT NULL PRIMARY KEY,
+    "short_url" TEXT NOT NULL,
     "origin" TEXT NOT NULL,
     "views" INTEGER NOT NULL DEFAULT 0,
-    "userId" TEXT,
-    "deletedAt" DATETIME,
+    "user_id" TEXT,
+    "deleted_at" DATETIME,
     "updated_at" DATETIME NOT NULL,
     "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "short_urls_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT "short_urls_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "short_urls_short_url_key" ON "short_urls"("short_url");
