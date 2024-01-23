9f37a9548d0d2dfdab95814e6d70f360920700c7
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2024-01-23 03:39:06 -0300

    style: change responses

--- src/lib/jwt/jwt.errors.ts
+++ src/lib/jwt/jwt.errors.ts
@@ -6,19 +6,19 @@ export function handleErrorsJwt(error?: JsonWebTokenError) {
 
   switch (error?.message) {
     case 'jwt expired':
-      message = 'Sua chave de acesso expirou.';
+      message = 'Seu token de autenticação expirou.';
       break;
     case 'jwt must be provided':
       message = 'Um token de autenticação pecisa ser informado.';
       break;
     case 'jwt malformed':
-      message = 'O token de autenticação tem um formato inválido.';
+      message = 'Seu token de autenticação tem um formato inválido.';
       break;
     case 'jwt not active':
       message = 'Seu token de autenticação ainda não está ativo.';
       break;
     default:
-      message = 'Verifique o token de autenticação.';
+      message = 'Verifique seu token de autenticação.';
   }
 
   throw new UnauthorizedException({ message });
