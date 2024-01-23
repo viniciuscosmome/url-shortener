d982b97b388839c006e9d04d0f9ccd424c1f452d
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2024-01-23 03:28:34 -0300

    refactor: move shortUrlCodeLength to constants

--- src/global/constants.ts
+++ src/global/constants.ts
@@ -1,4 +1,5 @@
 export const SESSION_PAYLOAD_KEY = 'session.payload';
 export const DESTINE_APLICATION_REDIR_URL = 'http://localhost:3000/';
 
+export const SHORT_URL_CODE_LENGTH = 6;
 export const SHORT_URL_REGEX = /^[a-zA-Z0-9]{6}$/;
--- src/modules/shortener/shortener.service.ts
+++ src/modules/shortener/shortener.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { DESTINE_APLICATION_REDIR_URL } from 'src/global/constants';
+import {
+  DESTINE_APLICATION_REDIR_URL,
+  SHORT_URL_CODE_LENGTH,
+} from 'src/global/constants';
 import type { GenerateShortUrlCodeResponse } from './shortener.types';
 
 @Injectable()
@@ -13,7 +16,7 @@ export class ShortenerService {
       'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789';
     let code = '';
 
-    for (let count = 0; count < 6; count++) {
+    for (let count = 0; count < SHORT_URL_CODE_LENGTH; count++) {
       const randomPos = Math.floor(Math.random() * alphanumeric.length);
       code += alphanumeric[randomPos];
     }
